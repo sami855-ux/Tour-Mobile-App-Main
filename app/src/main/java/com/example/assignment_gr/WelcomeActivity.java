44bcdd31207a338d0e5d8fe13df6e96239b5afc0
sami855-ux <samitale56@outlook.com>
2025-01-28 09:56:37 +0500

    WelcomeActivity.java is created

---
+++ app/src/main/java/com/example/assignment_gr/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.assignment_gr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.welcome_page);
+
+
+    }
+
+}
