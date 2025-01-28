6a2db825fca8de5ac1dadd3b5674c72737dba062
sami855-ux <samitale56@outlook.com>
2025-01-28 09:55:36 +0500

    LoginActivity.java is created

---
+++ app/src/main/java/com/example/assignment_gr/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.assignment_gr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login_page);
+
+
+    }
+
+}
