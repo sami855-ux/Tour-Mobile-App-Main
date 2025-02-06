ac4e34865bf6db7f7fd0848f1beb712f7b61be1b
sami855-ux <samitale56@outlook.com>
2025-02-06 21:33:28 +0500

    WelcomeActivity.java page is added

--- app/src/main/java/com/example/assignment_gr/WelcomeActivity.java
+++ app/src/main/java/com/example/assignment_gr/WelcomeActivity.java
@@ -7,15 +7,26 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private SessionManager sessionManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_page);
 
+        sessionManager = new SessionManager(this);
+
+        if (sessionManager.isLoggedIn()) {
+            // User is logged in, go to home screen
+            Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         Button getStartedButton = findViewById(R.id.started);
         getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 // Navigate to RegisterActivity
                 Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
                 startActivity(intent);
